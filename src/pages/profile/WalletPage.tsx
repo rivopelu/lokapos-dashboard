0012ee322cf3d899769d1919c7d33c70e7a89582
rivo pelu <dusty.emit@finestudio.org>
2025-02-03 14:30:08 +0700

    FEAT:add component popup page

--- src/pages/profile/WalletPage.tsx
+++ src/pages/profile/WalletPage.tsx
@@ -2,14 +2,49 @@ import { PageContainer } from '../../components/PageContainer.tsx';
 import { NumberFormatterHelper } from '../../helper/number-format-helper.ts';
 import { t } from 'i18next';
 import { useWalletPage } from './useWalletPage.ts';
-import { Button } from '@mui/material';
+import { Button, CardActionArea } from '@mui/material';
+import { PopupModal } from '../../components/PopupModal.tsx';
+import { InputRupiah } from '../../components/InputRupiah.tsx';
+import { CardBody, MainCard } from '../../components/MainLogo.tsx';
+import { useDataConstants } from '../../hooks/useDataConstants.ts';
 
 export function WalletPage() {
   const numberFormatHelper = new NumberFormatterHelper();
   const page = useWalletPage();
 
+  function modalTopUpComponent() {
+    return (
+      <div className={'grid gap-6'}>
+        <h1 className={'text-2xl capitalize'}>{t('top_up')}</h1>
+        <div>
+          <InputRupiah label={'amount'} />
+        </div>
+        <div>
+          <div className={"mb-2 text-slate-500"}>{t('select_bank')}</div>
+          <div className={'grid grid-cols-3 gap-3'}>
+            {useDataConstants().bankPaymentMethodData.map((item, i) => (
+              <CardActionArea key={i}>
+                <MainCard
+                  className={`px-10 border ${i.toString() === item.key ? 'border-primary-main bg-primary-main' : ''}`}
+                >
+                  <CardBody>
+                    <div className="flex items-center gap-4 justify-center">
+                      <img src={item.image} alt={item.name} className="h-10" />
+                    </div>
+                  </CardBody>
+                </MainCard>
+              </CardActionArea>
+            ))}
+          </div>
+        </div>
+        <Button variant={'contained'}>{t('top_up')}</Button>
+      </div>
+    );
+  }
+
   return (
     <PageContainer size={'xs'} className={'mt-8'}>
+      <PopupModal component={modalTopUpComponent()} open={true} />
       <div className={'grid gap-8 print:bg-red-500'}>
         <div className={'bg-white p-8 border rounded-lg'}>
           <div className={'grid gap-6'}>
@@ -18,7 +53,7 @@ export function WalletPage() {
               <div className={' text-4xl'}>{numberFormatHelper.toRupiah(page.walletBalance)}</div>
             </div>
             <div className={'grid grid-cols-2 gap-4'}>
-              <Button variant={"outlined"}>{t('top_up')}</Button>
+              <Button variant={'outlined'}>{t('top_up')}</Button>
             </div>
           </div>
         </div>
